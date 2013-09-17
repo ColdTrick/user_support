655c180ebb7dd1f014f9b32ec73c6c050f597645
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-17 13:53:28 +0200

    conditionally show contextual help

--- views/default/user_support/help_center.php
+++ views/default/user_support/help_center.php
@@ -4,6 +4,10 @@
 	$contextual_help_object = elgg_extract("contextual_help_object", $vars);
 	$faq = elgg_extract("faq", $vars);
 	
+	$help_enabled = false;
+	if (elgg_get_plugin_setting("help_enabled", "user_support") != "no") {
+		$help_enabled = true;
+	}
 ?>
 <div id="user_support_help_center">
 	<div id="user_support_help_center_header">
@@ -29,7 +33,7 @@
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:help_center:help_group"), "href" => $group->getURL(), "class" => "elgg-button elgg-button-action"));
 				}
 				
-				if(elgg_is_admin_logged_in() && empty($contextual_help_object)){
+				if(elgg_is_admin_logged_in() && empty($contextual_help_object) && $help_enabled){
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:help_center:help"), "href" => "#", "onclick" => "$('#user_support_help_edit_form_wrapper').toggle();$.fancybox.resize();", "class" => "elgg-button elgg-button-action"));
 				}
 			?>
@@ -39,7 +43,7 @@
 	</div>
 <?php
 
-	if(!empty($contextual_help_object)){
+	if(!empty($contextual_help_object) && $help_enabled){
 		echo elgg_view_module("info", "", elgg_view_entity($contextual_help_object), array("id" => "user_support_help_center_help", "class" => "mts"));
 	}
 	
@@ -47,7 +51,7 @@
 		echo elgg_view_module("info", elgg_echo("user_support:help_center:faq:title"), $faq, array("class" => "mts"));
 	}
 
-	if(elgg_is_admin_logged_in()){
+	if(elgg_is_admin_logged_in() && $help_enabled){
 		$form = elgg_view_form("user_support/help/edit", null, $vars);
 		echo elgg_view_module("info", elgg_echo("user_support:forms:help:title"), $form, array("id" => "user_support_help_edit_form_wrapper", "class" => "hidden mts"));
 	}
