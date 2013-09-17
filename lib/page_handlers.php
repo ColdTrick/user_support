4c40ce75598ff81e8e87ce87ce0363eb71b9d813
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-17 13:26:24 +0200

    prevent php notices about undefined offsets

--- lib/page_handlers.php
+++ lib/page_handlers.php
@@ -2,83 +2,89 @@
 	function user_support_page_handler($page){
 		$result = false;
 		
-		switch($page[0]){
-			case "help":
-				$result = true;
-				
-				include(dirname(dirname(__FILE__)) . "/pages/support_ticket/list.php");
-				break;
-			case "faq":
-				$result = true;
-				
-				switch($page[1]){
-					case "edit":
-						if(!empty($page[2]) && is_numeric($page[2])){
-							set_input("guid", $page[2]);
+		if (isset($page[0])) {
+			switch($page[0]){
+				case "help":
+					$result = true;
+					
+					include(dirname(dirname(__FILE__)) . "/pages/support_ticket/list.php");
+					break;
+				case "faq":
+					$result = true;
+					
+					if (isset($page[1])) {
+						switch($page[1]){
+							case "edit":
+								if(!empty($page[2]) && is_numeric($page[2])){
+									set_input("guid", $page[2]);
+								}
+								include(dirname(dirname(__FILE__)) . "/pages/faq/edit.php");
+								break;
+							case "group":
+								if (!empty($page[2]) && is_numeric($page[2])) {
+									elgg_set_page_owner_guid($page[2]);
+								}
+								include(dirname(dirname(__FILE__)) . "/pages/faq/group.php");
+								break;
+							case "add":
+								if (!empty($page[2]) && is_numeric($page[2])) {
+									elgg_set_page_owner_guid($page[2]);
+								}
+								include(dirname(dirname(__FILE__)) . "/pages/faq/add.php");
+								break;
+							default:
+								if(!empty($page[1]) && is_numeric($page[1])){
+									set_input("guid", $page[1]);
+									include(dirname(dirname(__FILE__)) . "/pages/faq/view.php");
+								} else {
+									include(dirname(dirname(__FILE__)) . "/pages/faq/list.php");
+								}
+								break;
 						}
-						include(dirname(dirname(__FILE__)) . "/pages/faq/edit.php");
-						break;
-					case "group":
-						if (!empty($page[2]) && is_numeric($page[2])) {
-							elgg_set_page_owner_guid($page[2]);
+					}
+					break;
+				case "support_ticket":
+					$result = true;
+					
+					if (isset($page[1])) {
+						switch($page[1]){
+							case "edit":
+								if(!empty($page[2]) && is_numeric($page[2])){
+									set_input("guid", $page[2]);
+								}
+								include(dirname(dirname(__FILE__)) . "/pages/support_ticket/edit.php");
+								break;
+							case "archive":
+								include(dirname(dirname(__FILE__)) . "/pages/support_ticket/archive.php");
+								break;
+							case "mine":
+								if(!empty($page[2]) && ($page[2] == "archive")){
+									set_input("status", UserSupportTicket::CLOSED);
+								}
+								include(dirname(dirname(__FILE__)) . "/pages/support_ticket/mine.php");
+								break;
+							default:
+								if(!empty($page[1]) && is_numeric($page[1])){
+									set_input("guid", $page[1]);
+									include(dirname(dirname(__FILE__)) . "/pages/support_ticket/view.php");
+								} else {
+									include(dirname(dirname(__FILE__)) . "/pages/support_ticket/list.php");
+								}
+								break;
 						}
-						include(dirname(dirname(__FILE__)) . "/pages/faq/group.php");
-						break;
-					case "add":
-						if (!empty($page[2]) && is_numeric($page[2])) {
-							elgg_set_page_owner_guid($page[2]);
-						}
-						include(dirname(dirname(__FILE__)) . "/pages/faq/add.php");
-						break;
-					default:
-						if(!empty($page[1]) && is_numeric($page[1])){
-							set_input("guid", $page[1]);
-							include(dirname(dirname(__FILE__)) . "/pages/faq/view.php");
-						} else {
-							include(dirname(dirname(__FILE__)) . "/pages/faq/list.php");
-						}
-						break;
-				}
-				break;
-			case "support_ticket":
-				$result = true;
-				
-				switch($page[1]){
-					case "edit":
-						if(!empty($page[2]) && is_numeric($page[2])){
-							set_input("guid", $page[2]);
-						}
-						include(dirname(dirname(__FILE__)) . "/pages/support_ticket/edit.php");
-						break;
-					case "archive":
-						include(dirname(dirname(__FILE__)) . "/pages/support_ticket/archive.php");
-						break;
-					case "mine":
-						if(!empty($page[2]) && ($page[2] == "archive")){
-							set_input("status", UserSupportTicket::CLOSED);
-						}
-						include(dirname(dirname(__FILE__)) . "/pages/support_ticket/mine.php");
-						break;
-					default:
-						if(!empty($page[1]) && is_numeric($page[1])){
-							set_input("guid", $page[1]);
-							include(dirname(dirname(__FILE__)) . "/pages/support_ticket/view.php");
-						} else {
-							include(dirname(dirname(__FILE__)) . "/pages/support_ticket/list.php");
-						}
-						break;
-				}
-				break;
-			case "help_center":
-				$result = true;
-				
-				include(dirname(dirname(__FILE__)) . "/pages/help_center.php");
-				break;
-			case "search":
-				$result = true;
-				
-				include(dirname(dirname(__FILE__)) . "/procedures/search.php");
-				break;
+					}
+					break;
+				case "help_center":
+					$result = true;
+					
+					include(dirname(dirname(__FILE__)) . "/pages/help_center.php");
+					break;
+				case "search":
+					$result = true;
+					
+					include(dirname(dirname(__FILE__)) . "/procedures/search.php");
+					break;
+			}
 		}
 		
 		return $result;
