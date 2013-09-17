13f2ca00004a69b650bc2370b8984963d1d954e2
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-17 15:07:21 +0200

    improved layout of contextual help

--- views/default/user_support/help_center.php
+++ views/default/user_support/help_center.php
@@ -22,18 +22,18 @@
 		<div id="user_support_help_center_actions">
 			<?php
 			
-				if(elgg_is_logged_in()){
+				if (elgg_is_logged_in()) {
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:help_center:ask"), "href" => "#","onclick" => "$('#user_support_ticket_edit_form_wrapper').toggle();$.fancybox.resize();", "class" => "elgg-button elgg-button-action"));
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:menu:support_tickets:mine"), "href" => $vars["url"] . "user_support/support_ticket/mine", "class" => "elgg-button elgg-button-action"));
 				}
 				
 				echo elgg_view("output/url", array("text" => elgg_echo("user_support:menu:faq"), "href" => $vars["url"] . "user_support/faq", "class" => "elgg-button elgg-button-action"));
 				
-				if(!empty($group)){
+				if (!empty($group)) {
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:help_center:help_group"), "href" => $group->getURL(), "class" => "elgg-button elgg-button-action"));
 				}
 				
-				if(elgg_is_admin_logged_in() && empty($contextual_help_object) && $help_enabled){
+				if (elgg_is_admin_logged_in() && empty($contextual_help_object) && $help_enabled && elgg_is_xhr()) {
 					echo elgg_view("output/url", array("text" => elgg_echo("user_support:help_center:help"), "href" => "#", "onclick" => "$('#user_support_help_edit_form_wrapper').toggle();$.fancybox.resize();", "class" => "elgg-button elgg-button-action"));
 				}
 			?>
@@ -43,20 +43,22 @@
 	</div>
 <?php
 
-	if(!empty($contextual_help_object) && $help_enabled){
-		echo elgg_view_module("info", "", elgg_view_entity($contextual_help_object), array("id" => "user_support_help_center_help", "class" => "mts"));
+	if (elgg_is_xhr()) {
+		if (!empty($contextual_help_object) && $help_enabled) {
+			echo elgg_view_module("info", elgg_echo("user_support:help_center:help:title"), elgg_view_entity($contextual_help_object, array("title" => "")), array("id" => "user_support_help_center_help", "class" => "mts"));
+		}
+		
+		if (elgg_is_admin_logged_in() && $help_enabled) {
+			$form = elgg_view_form("user_support/help/edit", null, $vars);
+			echo elgg_view_module("info", elgg_echo("user_support:forms:help:title"), $form, array("id" => "user_support_help_edit_form_wrapper", "class" => "hidden mts"));
+		}
 	}
 	
-	if(!empty($faq)){
+	if (!empty($faq)) {
 		echo elgg_view_module("info", elgg_echo("user_support:help_center:faq:title"), $faq, array("class" => "mts"));
 	}
 
-	if(elgg_is_admin_logged_in() && $help_enabled){
-		$form = elgg_view_form("user_support/help/edit", null, $vars);
-		echo elgg_view_module("info", elgg_echo("user_support:forms:help:title"), $form, array("id" => "user_support_help_edit_form_wrapper", "class" => "hidden mts"));
-	}
-	
-	if(elgg_is_logged_in()){
+	if (elgg_is_logged_in()) {
 		$form = elgg_view_form("user_support/support_ticket/edit", null, $vars);
 		echo elgg_view_module("info", elgg_echo("user_support:help_center:ask"), $form, array("id" => "user_support_ticket_edit_form_wrapper", "class" => "hidden mts"));
 	}
