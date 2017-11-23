4ff1690c3eb245660e7d0b106ac9f4a323dee0a9
Jerome Bakker <jeabakker@coldtrick.com>
2017-11-23 16:42:21 +0100

    chore: WIP code refactor user settings

--- views/default/plugins/user_support/usersettings.php
+++ views/default/plugins/user_support/usersettings.php
@@ -1,25 +1,24 @@
 <?php
 
 $page_owner = elgg_get_page_owner_entity();
-
-if ($page_owner->getGUID() != elgg_get_logged_in_user_guid()) {
+if (!$page_owner instanceof ElggUser || $page_owner->guid !== elgg_get_logged_in_user_guid()) {
 	return;
 }
 
-if (!user_support_staff_gatekeeper(false, $page_owner->getGUID())) {
+if (!user_support_staff_gatekeeper(false, $page_owner->guid)) {
 	return;
 }
 
-$plugin = elgg_extract("entity", $vars);
+/* @var $plugin ElggPlugin */
+$plugin = elgg_extract('entity', $vars);
 
-$noyes_options = array(
-	"no" => elgg_echo("option:no"),
-	"yes" => elgg_echo("option:yes")
-);
-
-$body = "<div>";
-$body .= elgg_echo("user_support:usersettings:admin_notify") . "<br />";
-$body .= elgg_view("input/dropdown", array("name" => "params[admin_notify]", "options_values" => $noyes_options, "value" => $plugin->getUserSetting("admin_notify", $page_owner->getGUID())));
-$body .= "</div>";
-
-echo $body;
+echo elgg_view_field([
+	'#type' => 'select',
+	'#label' => elgg_echo('user_support:usersettings:admin_notify'),
+	'name' => 'params[admin_notify]',
+	'value' => $plugin->getUserSetting('admin_notify', $page_owner->guid),
+	'options_values' => [
+		'no' => elgg_echo('option:no'),
+		'yes' => elgg_echo('option:yes'),
+	],
+]);
