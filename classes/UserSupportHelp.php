3f1610a19460160187167208f985f65976c84a9e
Jerome Bakker <jeabakker@coldtrick.com>
2018-01-08 12:32:46 +0100

    fixed: no comments on contextual help

--- classes/UserSupportHelp.php
+++ classes/UserSupportHelp.php
@@ -30,4 +30,10 @@ class UserSupportHelp extends ElggObject {
 		return elgg_normalize_url('user_support/help/view/' . $this->guid . '/' . elgg_get_friendly_title($this->getDisplayName()));
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function canComment($user_guid = 0, $default = null) {
+		return false;
+	}
 }
