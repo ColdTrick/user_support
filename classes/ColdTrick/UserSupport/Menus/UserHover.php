1a5982eb7c4e134792c0d4d1e01adccb0bcd4d97
Jerome Bakker <jeabakker@coldtrick.com>
2019-03-15 11:46:01 +0100

    chore: user hover menu items

--- classes/ColdTrick/UserSupport/Menus/UserHover.php
+++ classes/ColdTrick/UserSupport/Menus/UserHover.php
@@ -26,28 +26,35 @@ class UserHover {
 			return;
 		}
 		
+		if ($entity->isAdmin()) {
+			// admins are always support staff
+			return;
+		}
+		
 		$is_staff = user_support_staff_gatekeeper(false, $entity->guid);
 		
 		$return_value[] = \ElggMenuItem::factory([
 			'name' => 'user_support_staff_make',
+			'icon' => 'level-up-alt',
 			'text' => elgg_echo('user_support:menu:user_hover:make_staff'),
 			'href' => elgg_generate_action_url('user_support/support_staff', [
 				'guid' =>  $entity->guid,
 			]),
-			'confirm' => elgg_echo('question:areyousure'),
 			'section' => 'admin',
 			'item_class' => $is_staff ? 'hidden' : '',
+			'data-toggle' => 'user-support-staff-remove',
 		]);
 		
 		$return_value[] = \ElggMenuItem::factory([
 			'name' => 'user_support_staff_remove',
+			'icon' => 'level-down-alt',
 			'text' => elgg_echo('user_support:menu:user_hover:remove_staff'),
 			'href' => elgg_generate_action_url('user_support/support_staff', [
 				'guid' => $entity->guid,
 			]),
-			'confirm' => elgg_echo('question:areyousure'),
 			'section' => 'admin',
 			'item_class' => $is_staff ?: 'hidden',
+			'data-toggle' => 'user-support-staff-make',
 		]);
 		
 		return $return_value;
