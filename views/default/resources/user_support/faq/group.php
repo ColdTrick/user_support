2e4b32c906f055cb9185efc82f77f4e2c96068e9
Jerome Bakker <jeabakker@coldtrick.com>
2019-04-15 13:39:27 +0200

    chore: use correct gatekeeper

--- views/default/resources/user_support/faq/group.php
+++ views/default/resources/user_support/faq/group.php
@@ -3,15 +3,16 @@
 // get the page owner
 $group_guid = (int) elgg_extract('guid', $vars);
 
-elgg_group_gatekeeper();
-elgg_group_tool_gatekeeper('faq');
+elgg_entity_gatekeeper($group_guid, 'group');
+
+elgg_group_tool_gatekeeper('faq', $group_guid);
 
 $page_owner = elgg_get_page_owner_entity();
 
 // build breadcrumb
-elgg_push_collection_breadcrumbs('ojbect', 'faq', $page_owner);
+elgg_push_collection_breadcrumbs('object', \UserSupportFAQ::SUBTYPE, $page_owner);
 
-elgg_register_title_button('user_support', 'add', 'object', 'faq');
+elgg_register_title_button('user_support', 'add', 'object', \UserSupportFAQ::SUBTYPE);
 
 // build page elements
 $title_text = elgg_echo('user_support:faq:group:title', [$page_owner->getDisplayName()]);
