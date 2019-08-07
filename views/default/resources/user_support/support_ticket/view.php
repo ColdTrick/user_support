c9ff926120d4f82769ff55fa102b89c2fb93e9de
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-08-07 14:25:47 +0200

    chore: replaced elgg_set_ignore_access with elgg call logic

--- views/default/resources/user_support/support_ticket/view.php
+++ views/default/resources/user_support/support_ticket/view.php
@@ -1,37 +1,33 @@
 <?php
 
 $guid = (int) elgg_extract('guid', $vars);
+$title_text = '';
 
 // ignore access for support staff
-if (user_support_staff_gatekeeper(false)) {
-	$ia = elgg_set_ignore_access(true);
-}
+$flag = user_support_staff_gatekeeper(false) ? ELGG_IGNORE_ACCESS : 0;
 
-elgg_entity_gatekeeper($guid, 'object', UserSupportTicket::SUBTYPE);
-
-/* @var $entity \UserSupportTicket */
-$entity = get_entity($guid);
-
-// build page elements
-$title_text = elgg_echo("user_support:support_type:{$entity->getSupportType()}") . ': ' . $entity->getDisplayName();
-
-elgg_push_entity_breadcrumbs($entity);
-
-// show entity
-$content = elgg_view_entity($entity);
-
-// build page
-$page_data = elgg_view_layout('default', [
-	'title' => $title_text,
-	'content' => $content,
-	'filter' => false,
-	'entity' => $entity,
-]);
-
-// restore access
-if (user_support_staff_gatekeeper(false)) {
-	elgg_set_ignore_access($ia);
-}
+$page_data = elgg_call($flag, function() use ($guid, &$title_text) {
+	elgg_entity_gatekeeper($guid, 'object', UserSupportTicket::SUBTYPE);
+	
+	/* @var $entity \UserSupportTicket */
+	$entity = get_entity($guid);
+	
+	// build page elements
+	$title_text = elgg_echo("user_support:support_type:{$entity->getSupportType()}") . ': ' . $entity->getDisplayName();
+	
+	elgg_push_entity_breadcrumbs($entity);
+	
+	// show entity
+	$content = elgg_view_entity($entity);
+	
+	// build page
+	return elgg_view_layout('default', [
+		'title' => $title_text,
+		'content' => $content,
+		'filter' => false,
+		'entity' => $entity,
+	]);
+});
 
 // draw page
 echo elgg_view_page($title_text, $page_data);
